eb4da2ab2ee688b966ef44eb9faa81405e8a3e2c
DeadNeons <robshields@robshields.net>
2026-01-08 04:02:48 -0500

    Test 4.3

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Rob Shields-HM WebXR-0.1.0";
+const contentToCache = [
+    "Build/HMWebXR.loader.js",
+    "Build/HMWebXR.framework.js",
+    "Build/HMWebXR.data",
+    "Build/HMWebXR.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
